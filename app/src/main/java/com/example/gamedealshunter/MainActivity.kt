a958ea156423d4feaaae0af7b6bdad730b394b0a
tribor <6595932@gmail.com>
2025-05-23 18:20:30 +0300

    Скорректировал MainActivity.kt

--- app/src/main/java/com/example/gamedealshunter/MainActivity.kt
+++ app/src/main/java/com/example/gamedealshunter/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,7 +19,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GameDealsHunterTheme(dynamicColor = false) {
-                AppNavGraph()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color    = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavGraph()
+                }
             }
         }
     }
